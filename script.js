217cea7c34353cd55c6b214c18c2fd2d2efc82cd
robertcalimanp <contato.expressrcp@gmail.com>
2025-06-24 21:48:57 -0300

    criando portilofio

---
+++ script.js
@@ -0,0 +1,13 @@
+// script.js - funcionalidades futuras
+
+// Exemplo: Scroll suave para seções
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault();
+    const target = document.querySelector(this.getAttribute('href'));
+    if (target) {
+      target.scrollIntoView({ behavior: 'smooth' });
+    }
+  });
+});
